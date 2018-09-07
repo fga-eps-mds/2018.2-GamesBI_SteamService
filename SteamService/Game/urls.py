24e6adae1ad78cfbb2adf4f912c0da17e7c61345
alanrslima <alanronison.lima@gmail.com>
2018-09-07 07:55:26 -0300

    [FIX] fga-eps-mds/2018.2-GamesBI#78 Bug fix in url of the game

--- SteamService/Game/urls.py
+++ SteamService/Game/urls.py
@@ -2,5 +2,6 @@ from django.urls import include, path
 from .views import SteamView
 
 urlpatterns = [
-    path('get_steam_games_list/', SteamView.as_view(), name="get_steam_games"),
+    # path('get_steam_games_list/', SteamView.as_view(), name="get_steam_games"),
+    path('get_steam_games_list/', SteamView.as_view())
 ]
