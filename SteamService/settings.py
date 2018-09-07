c6e738bafcd77d1e4555f0aca62bdc142e8f1fcb
alanrslima <alanronison.lima@gmail.com>
2018-09-07 01:19:33 -0300

    [ADD] fga-eps-mds/2018.2-GamesBI#79 Created request to igdb and insert rest_framework to requirements.txt

--- SteamService/settings.py
+++ SteamService/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'SteamService.Game'
+    'SteamService.Game',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
