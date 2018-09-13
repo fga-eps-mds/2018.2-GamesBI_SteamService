42b329afc2b11afffee24b8a295734c567f2cad9
Andre Eduardo <andre.eduardo11@hotmail.com>
2018-09-13 15:55:00 -0300

    [UPDATE] fga-eps-mds/2018.2-GamesBI#79 removing test logs

--- SteamService/importdata/views.py
+++ SteamService/importdata/views.py
@@ -31,17 +31,6 @@ class SteamView(APIView):
         games = Game.objects.all()
         games_data = []
         for game in games:
-            print('------------')
-            print(game.id)
-            print(game.name)
-            print(game.positive_reviews_steam)
-            print(game.negative_reviews_steam)
-            print(game.average_forever)
-            print(game.average_2weeks)
-            print(game.price)
-            print(game.owners)
-            print(game.lenguages)
-            print('------------')
             game_data = {
                 'id': game.id,
                 'name': game.name,
@@ -56,7 +45,6 @@ class SteamView(APIView):
             games_data.append(game_data)
 
         return Response(data=games_data)
-
 
     def get_game_data(self, game_id):
         url = 'http://steamspy.com/api.php?request=appdetails&appid={}'.format(game_id)
@@ -126,28 +114,24 @@ class SteamView(APIView):
         }
         return filtered_data
 
-
     def save_game(self, filtered_data):
         new_game = Game(
-            id = filtered_data['id'],
-            name = filtered_data['name'],
-            positive_reviews_steam = filtered_data['positive_reviews_steam'],
-            negative_reviews_steam = filtered_data['negative_reviews_steam'],
-            owners = filtered_data['owners'],
-            average_forever = filtered_data['average_forever'],
-            average_2weeks = filtered_data['average_2weeks'],
-            price = filtered_data['price'],
-            lenguages = filtered_data['lenguages'],
+            id=filtered_data['id'],
+            name=filtered_data['name'],
+            positive_reviews_steam=filtered_data['positive_reviews_steam'],
+            negative_reviews_steam=filtered_data['negative_reviews_steam'],
+            owners=filtered_data['owners'],
+            average_forever=filtered_data['average_forever'],
+            average_2weeks=filtered_data['average_2weeks'],
+            price=filtered_data['price'],
+            lenguages=filtered_data['lenguages'],
         )
         new_game.save()
-
-
 
     def read_owners(self, str_owners):
         vector_numbers = self.valid_owners(str_owners)
         average = self.calculates_avarege(vector_numbers)
         return average
-
 
     def valid_owners(self, str_owners):
         low_average = str_owners.split(" .. ")[0]
