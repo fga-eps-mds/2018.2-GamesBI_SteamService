0f9563a9a05cea2260d365b33d2de2df28eba203
Alan Lima <alanronison.lima@gmail.com>
2018-09-14 20:52:17 -0300

    [DELETE] fga-eps-mds/2018.2-GamesBI#79 Deleted all unused files

--- SteamService/API/json/viewsets.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from SteamService.importdata.models import Game
-from .serializers import GamesSerializer
-
-class GamesViewSet(ModelViewSet):
-    queryset = Game.objects.all()
-    serializer_class = GamesSerializer
